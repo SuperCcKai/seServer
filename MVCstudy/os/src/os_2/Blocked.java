427a0846efb68c4b226911a647efb90cca9d4fc9
supercckai <2574755954@qq.com>
2018-05-21 19:22:26 +0800

    delete

--- MVCstudy/os/src/os_2/Blocked.java
+++
@@ -1,7 +0,0 @@
-package os_2;
-
-import java.util.LinkedList;
-
-public class Blocked {
-	public static LinkedList<process> list = new LinkedList<process>();
-}
--- MVCstudy/os/src/os_2/Ready.java
+++
@@ -1,7 +0,0 @@
-package os_2;
-
-import java.util.LinkedList;
-
-public class Ready {
-	public static LinkedList<process> list = new LinkedList<process>();
-}
